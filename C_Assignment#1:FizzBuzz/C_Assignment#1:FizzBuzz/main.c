53faf018b185e2ac38c6d74546bc7edbbb081a3e
NiNajamie <NiNajamie@users.noreply.github.com>
2016-03-05 00:02:09 -0800

    Added files via upload

---
+++ C_Assignment#1:FizzBuzz/C_Assignment#1:FizzBuzz/main.c
@@ -0,0 +1,26 @@
+//
+//  main.c
+//  C_Assignment#1:FizzBuzz
+//
+//  Created by Asuka Nakagawa on 2016-03-03.
+//  Copyright © 2016 Asuka Nakagawa. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    
+    int num = 0;
+    for (int i = 0; i < 100; i++) {
+        num++;
+        if (num % 3 == 0) {
+            printf("Fizz\n");
+        }else if (num % 5 == 0) {
+            printf("Buzz\n");
+        }else if ((num % 3 == 0) && (num % 5 == 0)) {
+            printf("FizzBuzz\n");
+        }else {
+            printf("%d\n", num);
+        }
+    }return 0;
+}
