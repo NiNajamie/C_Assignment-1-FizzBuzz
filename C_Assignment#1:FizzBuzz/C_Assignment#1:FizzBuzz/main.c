9c44f80026e59772edea07f2d0f39e1f8d399b93
NiNajamie <NiNajamie@users.noreply.github.com>
2016-03-09 18:41:30 -0800

    Fixed

--- C_Assignment#1:FizzBuzz/C_Assignment#1:FizzBuzz/main.c
+++ C_Assignment#1:FizzBuzz/C_Assignment#1:FizzBuzz/main.c
@@ -13,14 +13,16 @@ int main(int argc, const char * argv[]) {
     int num = 0;
     for (int i = 0; i < 100; i++) {
         num++;
-        if (num % 3 == 0) {
-            printf("Fizz\n");
+        
+        if (num % 15 == 0) {
+            printf("FizzBuzz\n");
         }else if (num % 5 == 0) {
             printf("Buzz\n");
-        }else if ((num % 3 == 0) && (num % 5 == 0)) {
-            printf("FizzBuzz\n");
+        }else if (num % 3 == 0) {
+            printf("Fizz\n");
         }else {
             printf("%d\n", num);
         }
     }return 0;
+    
 }
